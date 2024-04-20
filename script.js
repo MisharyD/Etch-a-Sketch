73ade8d4cf487d430fdb44c319516ec7ead4fa34
MisharyD <masharyahmed123@gmail.com>
2024-04-20 14:45:35 +0300

    add changing and reverting color functionality I was strugling with the setTimeout function because it was not delaying the execution of the revert color function. Turns out If you use () after a function name that means to execute it right NOW. If you use just the function name or an anonymous function with (), that's like passing a pointer to the function. When passing a function as an argument to any other function, you always want just the name or an anonymous function.

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
-let container = document.querySelector(".container");
+let container = document.querySelector(".grid-container");
 let gridSize = 16;
 
+initGrid();
 function initGrid()
 {
     for(let i=0; i<gridSize; i++)
     {
         let row = document.createElement("div")
+        row.classList.add("row")
         for(let j=0; j<gridSize; j++)
         {
             let box = document.createElement("div");
             box.setAttribute("id", i+"" + j)
             box.classList.add("box")
             box.setAttribute("style", "width: " + gridSize *2 + "px;" + "height: " + gridSize *2 + "px;")
+            box.addEventListener("mouseover",changeColor);
+            box.addEventListener("mouseleave",function(e)
+            {
+                setTimeout(() => revertColor(e),1000);
+            })
             row.appendChild(box);
         }
         container.appendChild(row);
     }
-    container.addEventListener("mouseover",changeColor);
 }
-
-initGrid();
 
 function changeColor(e)
 {
     let box = e.target;
     let randomColor = Math.floor(Math.random()*16777215).toString(16);
-    box.setAttribute("style", "background-color: " + randomColor);
+    box.style.backgroundColor = "#" + randomColor;
+}
+
+function revertColor(e)
+{
+    let box = e.target;
+    box.style.backgroundColor = "black";
 }
 
