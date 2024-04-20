77bdfb4f76875faee0bdf83d76a69f719b4c080a
MisharyD <masharyahmed123@gmail.com>
2024-04-20 13:50:18 +0300

    cod some strange behaviour that needs to be looked at later

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 let container = document.querySelector(".container");
+let gridSize = 16;
 
 function initGrid()
 {
-    let gridSize = 16;
-for(let i=0; i<gridSize; i++)
-{
-    let row = document.createElement("div")
-    for(let j=0; j<gridSize; j++)
+    for(let i=0; i<gridSize; i++)
     {
-        let column = document.createElement("div");
-        column.setAttribute("id", i+"" + j)
-        column.classList.add("box")
-        column.setAttribute("style", "width: " + gridSize + "px;" + "height: " + gridSize + "px;")
-        row.appendChild(column);
+        let row = document.createElement("div")
+        for(let j=0; j<gridSize; j++)
+        {
+            let box = document.createElement("div");
+            box.setAttribute("id", i+"" + j)
+            box.classList.add("box")
+            box.setAttribute("style", "width: " + gridSize *2 + "px;" + "height: " + gridSize *2 + "px;")
+            row.appendChild(box);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
-}
+    container.addEventListener("mouseover",changeColor);
 }
 
+initGrid();
 
+function changeColor(e)
+{
+    let box = e.target;
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    box.setAttribute("style", "background-color: " + randomColor);
+}
 
