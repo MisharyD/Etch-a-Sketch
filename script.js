1414fb7a883668410b3d4f84fc3f26b4b0742eb2
MisharyD <masharyahmed123@gmail.com>
2024-04-20 15:20:47 +0300

    add change grid size functionality

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-let container = document.querySelector(".grid-container");
-let gridSize = 16;
+let gridContainer = document.querySelector(".grid-container");
+let gridRows = 24;
+let gridColumns = 24;
+let changeButton = document.querySelector(".changeSize")
+changeButton.addEventListener("click",changeGridSize);
 
 initGrid();
 function initGrid()
 {
-    for(let i=0; i<gridSize; i++)
+    for(let i=0; i<gridRows; i++)
     {
         let row = document.createElement("div")
         row.classList.add("row")
-        for(let j=0; j<gridSize; j++)
+        for(let j=0; j<gridColumns; j++)
         {
             let box = document.createElement("div");
             box.setAttribute("id", i+"" + j)
             box.classList.add("box")
-            box.setAttribute("style", "width: " + gridSize *2 + "px;" + "height: " + gridSize *2 + "px;")
+            box.setAttribute("style", "width: " + 32  + "px;" + "height: " + 32  + "px;")
             box.addEventListener("mouseover",changeColor);
             box.addEventListener("mouseleave",function(e)
             {
@@ -21,7 +24,7 @@ function initGrid()
             })
             row.appendChild(box);
         }
-        container.appendChild(row);
+        gridContainer.appendChild(row);
     }
 }
 
@@ -38,3 +41,35 @@ function revertColor(e)
     box.style.backgroundColor = "black";
 }
 
+function changeGridSize()
+{
+    let rows = window.prompt("Number of rows:")
+    if(rows > 28)
+    {
+        window.alert("Number must be less than or equal to 28")
+        setTimeout(changeGridSize,5000)
+        return;
+    }
+    let columns = window.prompt("Number of columns:")
+    if(columns > 28)
+    {
+        window.alert("Number must be less than or equal to 28")
+        setTimeout(changeGridSize,5000)
+        return;
+    }
+
+    gridRows = rows;
+    gridColumns = columns;
+    removeCurrentGrid();
+    initGrid(); 
+}
+
+//removes the current grid container and add a new empty
+function removeCurrentGrid()
+{
+    let body = document.querySelector(".body");
+    body.removeChild(gridContainer);
+    gridContainer = document.createElement("div");
+    gridContainer.classList.add("grid-container");
+    body.appendChild(gridContainer)
+}
